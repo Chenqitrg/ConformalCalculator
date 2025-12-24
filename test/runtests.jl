7a6c430ca2b70eeadcbde4229a92ab29e11f8cb5
Chenqi Meng <mcqgravity@gmail.com>
2025-12-24 23:55:30 +0900

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -350,11 +350,14 @@ fs = [
     HoloPoly{Int,Float64}([1, 2, 3, 4, 5, 6, 7], [1.0, 4, 6, 4, 1, 0.5, 5.5], O{Int}(8)),
     HoloPoly{Int,Float64}([1, 2, 3, 4, 5, 6, 7], [1.0, 4, 6, 4, 1, 2, 3], O{Int}(8)),
     HoloPoly{Int,Float64}([1, 2, 3, 4], [1.0, 4, 6, 4], O{Int}(6)),
+    HoloPoly{Int,Float64}([1, 2, 4, 5], [1.0, 4.5, 6.5, 4], O{Int}(6)),
+    HoloPoly{Int,Float64}([1, 3, 4, 7], [1.0, 4.5, 6.5, 4], O{Int}(8)),
+    HoloPoly{Int,Float64}([1, 3, 4, 7, 8, 9, 12], [1.0, 4.5, 6.5, 4, 1, 1, 1], O{Int}(15)),
 ]
 
 @testset "Consistency of exponentialization and deexponentialization" begin
     for f in fs
-        for truncdim in 1:10
+        for truncdim in 1:20
             println("Designed truncation dimension of Lie generator: $truncdim")
             coeff, trunc = deexponentialize(f, O{Int}(truncdim))
             println("The calculated Witt algebra generator coefficient: $coeff")
